8c5943935af4803945f4be31de35e9162549fb46
Giovanni Bruno <g.bruno@arduino.cc>
2024-01-16 15:38:57 +0100

    1.0.0

--- examples/readValues/readValues.ino
+++ examples/readValues/readValues.ino
@@ -1,3 +1,15 @@
+/*
+
+	Arduino MAX17332 library
+
+	Copyright (c) 2023 Arduino SA
+
+    This Source Code Form is subject to the terms of the Mozilla Public
+    License, v. 2.0. If a copy of the MPL was not distributed with this
+    file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+*/
+
 #include <Arduino_MAX17332.h>
 #include <Wire.h>
 
